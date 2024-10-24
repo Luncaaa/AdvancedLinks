47583ae65b28961d6c597185e260b986b8a3b042
Luncaaa <luca.alavoir@gmail.com>
2024-10-24 22:09:32 +0200

    [1.3] - 1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,19 @@ plugins {
 }
 
 group = "me.lucaaa"
-version = "1.2"
+version = "1.3"
 
 repositories {
     mavenCentral()
     mavenLocal()
     maven("https://oss.sonatype.org/content/repositories/snapshots")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
     implementation("net.kyori:adventure-api:4.17.0")
     implementation("net.kyori:adventure-text-minimessage:4.17.0")
     implementation("net.kyori:adventure-text-serializer-legacy:4.17.0")
